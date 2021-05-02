d0256f9590d318de65e913eaf4c8c748efaebe66
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-05-02 11:54:18 -0300

    Add header component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
   <div id="app">
-    
+    <Header></Header>
   </div>
 </template>
 
-<style>
+<script>
+  import Header from './components/Header.vue';
 
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style>
+  html {
+    font-size: 10px;
+  }
+
+  body {
+    background-color: #1B1E1F;
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+    border: 0;
+    outline: 0;
+    font-family: 'Montserrat', sans-serif;
+    text-decoration: none;
+    box-sizing: border-box;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <div class="header-content">
+      <div class="logo">
+        <router-link to="/">
+          <img src="../assets/logo.png" alt="Repeteco">
+        </router-link>
+      </div>
+
+      <a href="https://github.com/filipealvess/repeteco" target="_blank">
+        <i class="fab fa-github github-link"></i>
+      </a>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style scoped>
+  header {
+    background-color: #121413;
+    padding: 10px 0;
+  }
+
+  .header-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 85%;
+    margin: 0 auto;
+  }
+
+  .logo img {
+    height: 3.2rem;
+  }
+
+  .github-link {
+    color: #B1B7B3;
+    font-size: 2.4rem;
+    transition: 0.2s color;
+  }
+
+  .github-link:hover {
+    color: #25F277;
+  }
+</style>
